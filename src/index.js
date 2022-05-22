3c52ed54b39886703ddb59f0d3efbd36fe9368c2
palmermolly <92853563+palmermolly@users.noreply.github.com>
2022-05-22 11:49:54 +0100

    Added icons to images folder
    Added ul/li styling to links. Played around with font-size and CSS positioning.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
 import "./styles.css";
+
+<form id="contact-form">
+  <p>FILL ME IN</p>
+  <input type="text" placeholder="NAME*" />
+  <input type="text" placeholder="EMAIL*" />
+  <input type="text" placeholder="PHONE NUMBER*" />
+  <input type="text" placeholder="COMPANY*" />
+  <input type="text" placeholder="MESSAGE*" />
+  <input type="submit" value="SUBMIT" />
+</form>;
